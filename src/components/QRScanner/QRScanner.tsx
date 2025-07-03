a12a9849567deed4d71c65e1c94ba73866dbaf3c
Kirill Romanov <kirillromanov@MacBook-Air-Kirill.local>
2025-07-04 01:19:02 +0300

    fix: fix styles qrcode scanner

--- src/components/QRScanner/QRScanner.tsx
+++ src/components/QRScanner/QRScanner.tsx
@@ -1,5 +1,5 @@
+import { Html5Qrcode } from "html5-qrcode";
 import { useEffect, useRef } from "react";
-import { Html5QrcodeScanner } from "html5-qrcode";
 import "./QRScanner.scss";
 import Icon from "../ui/Icon";
 
@@ -9,37 +9,63 @@ interface QRScannerProps {
 }
 
 const QRScanner: React.FC<QRScannerProps> = ({ onScan, onClose }) => {
-  const scannerRef = useRef<HTMLDivElement>(null);
+  const qrRef = useRef<HTMLDivElement>(null);
+  const html5QrCodeRef = useRef<Html5Qrcode | null>(null);
 
   useEffect(() => {
-    const scanner = new Html5QrcodeScanner(
-      "qr-reader",
-      { fps: 10, qrbox: 250 },
-      false
-    );
+    const initScanner = async () => {
+      const devices = await Html5Qrcode.getCameras();
 
-    scanner.render(
-      (decodedText) => {
-        onScan(decodedText);
-        scanner.clear().catch(console.error);
-      },
-      () => {
-        // игнорируем ошибки
+      if (devices && devices.length) {
+        // Найдем заднюю камеру (если возможно)
+        const backCamera = devices.find((d) =>
+          d.label.toLowerCase().includes("back")
+        ) || devices[0]; // иначе первая доступная
+
+        const html5QrCode = new Html5Qrcode("qr-reader");
+        html5QrCodeRef.current = html5QrCode;
+
+        html5QrCode
+          .start(
+            backCamera.id,
+            { fps: 10, qrbox: 250 },
+            (decodedText) => {
+              onScan(decodedText);
+              html5QrCode.stop().then(() => {
+                html5QrCode.clear();
+              });
+            },
+            () => {} // onError
+          )
+          .catch(console.error);
       }
-    );
+    };
+
+    initScanner();
 
     return () => {
-      scanner.clear().catch(console.error);
+      if (html5QrCodeRef.current) {
+        html5QrCodeRef.current
+          .stop()
+          .then(() => html5QrCodeRef.current?.clear())
+          .catch(console.error);
+      }
     };
   }, [onScan]);
 
   return (
     <div className="qr-screen">
       <button className="qr-back" onClick={onClose}>
-        <Icon name="chevron-left" size={24}/>
+        <Icon name="chevron-left" size={24} />
       </button>
-      <div className="qr-title">Сканировать<br />QR код</div>
-      <div id="qr-reader" className="qr-reader" ref={scannerRef}></div>
+      <div className="qr-title">
+        Сканировать<br />QR код
+      </div>
+      <div className="qr-reader-container">
+      <div id="qr-reader" className="qr-reader" ref={qrRef}></div>
+      <img src="/icons/qr-frame.svg" alt="QR Frame" className="qr-frame-overlay" />
+      </div>
+      
     </div>
   );
 };
