0666638149fdae32f407fd4050919211c843ba98
Kirill Romanov <kirillromanov@MacBook-Air-Kirill.local>
2025-07-05 17:31:17 +0300

    fix: make fix ExitModal style

--- src/components/ExitModal/ExitModal.tsx
+++ src/components/ExitModal/ExitModal.tsx
@@ -13,16 +13,16 @@ export default function ExitModal({ isOpen, onClose, onLogout, onCancel }: Props
   if (!isOpen) return null;
 
   return (
-    <div className="modal-backdrop" onClick={onClose}>
-      <div className="modal-container" onClick={(e) => e.stopPropagation()}>
-        <div className="modal__header">
-        <button className="modal__close" onClick={onClose}>
+    <div className="exit-modal-backdrop" onClick={onClose}>
+      <div className="exit-modal-container" onClick={(e) => e.stopPropagation()}>
+        <div className="exit-modal__header">
+        <button className="exit-modal__close" onClick={onClose}>
             <Icon name="cros" />
           </button>
           <h1>Выйти из аккаунта?</h1>
         </div>
 
-        <form className="modal__form">
+        <form className="exit-modal__form">
             <Button onClick={onCancel}>Отмена</Button>
             <Button onClick={onLogout}>Выйти</Button>
         </form>
