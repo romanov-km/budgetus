231c9f7932956c3873f859449b9a910d12f2f88f
Kirill Romanov <kirillromanov@MacBook-Air-Kirill.local>
2025-07-08 18:42:18 +0300

    feat: add select icon category

--- src/components/AddCategoryModal/AddCategoryModal.tsx
+++ src/components/AddCategoryModal/AddCategoryModal.tsx
@@ -7,43 +7,137 @@ import { useState } from "react";
 interface Props {
   isOpen: boolean;
   onClose: () => void;
-  onAddCategory: (category: { name: string; icon: string, color: string }) => void;
+  onAddCategory: (category: {
+    name: string;
+    icon: string;
+    color: string;
+  }) => void;
 }
 
-export default function AddCategoryModal({ isOpen, onClose, onAddCategory }: Props) {
-    const [name, setName] = useState("");
+const iconOptions = [
+  "tour-bus",
+  "bone",
+  "shopping-cart",
+  "bell",
+  "plane",
+  "fork-knife",
+  "car",
+  "cloud-upload",
+  "email",
+  "mic",
+  "reports",
+  "trash",
+];
 
-    const handleSubmit = (e: React.FormEvent) => {
-      e.preventDefault();
-      if (!name.trim()) return;
-      onAddCategory({ name: name.trim(), icon: "tour-bus", color: "yellow"}); // icon временно дефолтная
-      setName("");
-      onClose();
-    };
-  
-  
+export default function AddCategoryModal({
+  isOpen,
+  onClose,
+  onAddCategory,
+}: Props) {
+  const [step, setStep] = useState<1 | 2 | 3>(1);
+  const [name, setName] = useState("");
+  const [icon, setIcon] = useState("tour-bus");
+  const [color, setColor] = useState("yellow");
+
+  const handleNext = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (step === 1 && name.trim()) setStep(2);
+    else if (step === 2 && icon) setStep(3);
+  };
+
+  const handleSubmit = () => {
+    if (!name || !icon || !color) return;
+    onAddCategory({ name, icon, color });
+    resetAndClose();
+  };
+
+  const resetAndClose = () => {
+    setStep(1);
+    setName("");
+    setIcon("");
+    setColor("#b9fe66");
+    onClose();
+  };
+
+  const handleBack = () => {
+    if (step > 1) setStep(step - 1);
+  };
+
   if (!isOpen) return null;
 
   return (
-    <div className="modal-backdrop" onClick={onClose}>
+    <div className="modal-backdrop" onClick={resetAndClose}>
       <div className="modal-container" onClick={(e) => e.stopPropagation()}>
         <div className="modal__header">
-          <button className="modal__close" onClick={onClose}>
+          <button className="modal__close" onClick={resetAndClose}>
             <Icon name="cros" />
           </button>
-          <h2>Добавление категории</h2>
+          <h2>
+            {step === 1 && "Добавление категории"}
+            {step === 2 && "Выбор иконки"}
+            {step === 3 && "Выбор цвета"}
+          </h2>
         </div>
-        <form className="modal__form" onSubmit={handleSubmit}>
-          <Input
-            label="Название категории"
-            name="category"
-            placeholder="Введите название"
-            onChange={(e) => setName(e.target.value)}
-            value={name}
-          />
 
-          <Button type="submit">Добавить</Button>
-        </form>
+        <div className="modal__content">
+          {step === 1 && (
+            <form className="modal__form" onSubmit={handleNext}>
+              <Input
+                label="Название категории"
+                name="category"
+                placeholder="Введите название"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+              />
+              <Button type="submit">Далее</Button>
+            </form>
+          )}
+
+          {step === 2 && (
+              <form className="modal_form" onSubmit={handleNext}>
+              <div className="icon-grid">
+                {iconOptions.map((i) => (
+                  <button
+                    key={i}
+                    className={`icon-option ${icon === i ? "selected" : ""}`}
+                    onClick={() => setIcon(i)}
+                  >
+                    <Icon name={i} />
+                  </button>
+                ))}
+              </div>
+              <div className="modal__buttons">
+                <Button variant="primary" onClick={handleBack}>
+                  Назад
+                </Button>
+                <Button type="submit">Далее</Button>
+              </div>
+              </form>
+          )}
+
+          {step === 3 && (
+            <>
+              <div className="color-picker">
+                <input
+                  type="color"
+                  value={color}
+                  onChange={(e) => setColor(e.target.value)}
+                />
+                <Input
+                  placeholder="Введите название в формате Hex"
+                  value={color}
+                  onChange={(e) => setColor(e.target.value)}
+                />
+              </div>
+              <div className="modal__buttons">
+                <Button variant="primary" onClick={handleBack}>
+                  Назад
+                </Button>
+                <Button onClick={handleSubmit}>Добавить</Button>
+              </div>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
